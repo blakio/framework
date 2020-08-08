9ffdc961c331b0c6b9ecf6071bcee977a1731c22
Isaiah Harrison <ijh5005@outlook.com>
2020-08-07 21:55:09 -0400

    folder file name change

--- src/App.js
+++ src/App.js
@@ -2,69 +2,78 @@ import React, {
   useEffect
 } from 'react';
 import './App.css';
-import b from "./blakio";
+import {
+  SideBar,
+  TopBar,
+  Dashboard,
+  LoadingScreen,
+  LogIn
+} from "./blakio_home";
 
-// import ReactNotification from 'react-notifications-component'
-// import 'react-notifications-component/dist/theme.css';
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css';
 
-// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
-// import {
-//   StateProvider,
-//   StateContext
-// } from "blakio_context/State";
-// import Types from 'blakio_context/Types';
-// import Axios from 'blakio_axios';
+import {
+  StateProvider,
+  StateContext
+} from "blakio_context/State";
+import Types from 'blakio_context/Types';
+import Axios from 'blakio_axios';
 
-// const Loading = () => {
-//   const [state, dispatch] = StateContext();
-//   return state.isLoading && <LoadingScreen />
-// }
+const Loading = () => {
+  const [state, dispatch] = StateContext();
+  return state.isLoading && <LoadingScreen />
+}
 
-// const ContentArea = () => {
-//   const [state, dispatch] = StateContext();
+const ContentArea = () => {
+  const [state, dispatch] = StateContext();
 
-//   return (<div id="ContentArea" className={`${state.sideBarOptions.shortMenu && "shortMenu"}`}>
-//     <TopBar />
-//     <Dashboard />
-// </div>)
-// }
+  return (<div id="ContentArea" className={`${state.sideBarOptions.shortMenu && "shortMenu"}`}>
+    <TopBar />
+    <Dashboard />
+</div>)
+}
 
-// const Content = () => {
+const Content = () => {
 
-//   const [state, dispatch] = StateContext();
+  const [state, dispatch] = StateContext();
 
-//   useEffect(() => {
-//     const store = localStorage.getItem("blakio_store");
-//     if(store){
-//       const checkLocalStorage = true;
-//       Axios.logIn(store, checkLocalStorage).then(data => {
-//         if(data.data.logIn){
-//           dispatch({
-//             type: Types.IS_LOGGED_IN,
-//             payload: true
-//           })
-//         } else {
-//           localStorage.removeItem("blakio_store")
-//         }
-//       }).catch(err => console.log(err))
-//     }
-//   }, []);
+  useEffect(() => {
+    const store = localStorage.getItem("blakio_store");
+    if(store){
+      const checkLocalStorage = true;
+      Axios.logIn(store, checkLocalStorage).then(data => {
+        if(data.data.logIn){
+          dispatch({
+            type: Types.IS_LOGGED_IN,
+            payload: true
+          })
+        } else {
+          localStorage.removeItem("blakio_store")
+        }
+      }).catch(err => console.log(err))
+    }
+  }, []);
 
-//   return (<div>
-//     {!state.isLoggedIn ? 
-//      <LogIn /> : (
-//       <div id="App">
-//         <SideBar />
-//         <ContentArea />
-//         <Loading />
-//       </div>
-//      )}
-//   </div>)
-// }
+  return (<div>
+    {!state.isLoggedIn ? 
+     <LogIn /> : (
+      <div id="App">
+        <SideBar />
+        <ContentArea />
+        <Loading />
+      </div>
+     )}
+  </div>)
+}
 
 function App() {
-  return (<div>test</div>);
+  return (<StateProvider>
+    <ReactNotification />
+    <Content />
+  </StateProvider>);
 }
 
 export default App;
