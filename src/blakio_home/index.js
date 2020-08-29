424adc022b3dfaa2fbecae840d584578cbd2ce95
Isaiah Harrison <ijh5005@outlook.com>
2020-08-28 23:14:32 -0400

    maximizing space

--- src/blakio_home/index.js
+++ src/blakio_home/index.js
@@ -135,10 +135,8 @@ const TimeSheet = props => {
 const EmployeeDirectory = props => {
   if(!props.show) return <div></div>;
   return (<div>
-    <Grid grid="1">
-      <EmployeeTable />
-    </Grid>
     <Grid grid="2">
+      <EmployeeTable />
       <EmployeeForm />
     </Grid>
   </div>)
