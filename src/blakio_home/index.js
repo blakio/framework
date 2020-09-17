ae8cf894358c809f6e6d61b93ce7adf15cd4d39c
Isaiah Harrison <ijh5005@outlook.com>
2020-09-16 21:15:14 -0400

    testing fix

--- src/blakio_home/index.js
+++ src/blakio_home/index.js
@@ -53,24 +53,24 @@ const load = (dispatch, bool) => {
 }
 
 const SideBarHead = () => {
-  // const [state, dispatch] = StateContext();
-  // const {
-  //   shortMenu
-  // } = state.sideBarOptions;
+  const [state, dispatch] = StateContext();
+  const {
+    shortMenu
+  } = state.sideBarOptions;
 
-  // const onClick = () => {
-  //   dispatch({
-  //     type: Types.SHORT_MENU,
-  //     payload: !shortMenu
-  //   })
-  // }
+  const onClick = () => {
+    dispatch({
+      type: Types.SHORT_MENU,
+      payload: !shortMenu
+    })
+  }
 
-  // useEffect(() => {
-  //   Util.openPhoneMode(dispatch, true);
-  // }, []);
+  useEffect(() => {
+    Util.openPhoneMode(dispatch, true);
+  }, []);
 
-  // const margin = shortMenu ? "0 auto" : "0 0.58em";
-  // const width = shortMenu ? "80%" : null;
+  const margin = shortMenu ? "0 auto" : "0 0.58em";
+  const width = shortMenu ? "80%" : null;
 
   // return (<div id="SideBarHead" className={`flex ${shortMenu && "shortMenu"}`}>
   return (<div id="SideBarHead">
